4af2c03a828d44b8b0a957c72ffcc35500034c67
MusharratZ <miusharrat.parveen14@spsmail.cuny.edu>
2023-09-10 23:05:28 -0400

    Complete part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,18 @@
-
 class Book:
-    def __init__(self, author, title):
-        pass
-    
-    def display(self):
-        pass
+    def __init__(self, author="", title=""):
+        self.author = author
+        self.title = title
 
+    def display(self):
+        print(f"{self.title}, written by {self.author}")
+
+
+book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+book2 = Book("Walter Scott", "Ivanhoe: A Romance")
 
 if __name__ == "__main__":
-    pass
+    book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+
+    book1.display()
+    book2.display()
